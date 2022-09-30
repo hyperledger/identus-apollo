1b13232ed411502604b41ab5e7b5b27c275095f6
Ahmed Moussa <ahmed.moussa@iohk.io>
2022-09-30 05:59:49 +0200

    refactor: improve code
    Signed-off-by: Ahmed Moussa <ahmed.moussa@iohk.io>

--- Apollo/src/iosMain/kotlin/io/iohk/prism/apollo/Platform.kt
+++ Apollo/src/iosMain/kotlin/io/iohk/prism/apollo/Platform.kt
@@ -3,5 +3,5 @@ package io.iohk.prism.apollo
 import platform.UIKit.UIDevice
 
 actual object Platform {
-    actual val OS: String = UIDevice.currentDevice.systemName() + " " + UIDevice.currentDevice.systemVersion
+    actual val OS: String = "${UIDevice.currentDevice.systemName()}-${UIDevice.currentDevice.systemVersion}"
 }
--- Apollo/src/iosMain/kotlin/io/iohk/prism/apollo/ToPlatform.kt
+++ Apollo/src/iosMain/kotlin/io/iohk/prism/apollo/ToPlatform.kt
@@ -3,6 +3,7 @@ package io.iohk.prism.apollo
 import kotlinx.cinterop.UnsafeNumber
 import kotlinx.cinterop.addressOf
 import kotlinx.cinterop.allocArrayOf
+import kotlinx.cinterop.convert
 import kotlinx.cinterop.memScoped
 import kotlinx.cinterop.usePinned
 import platform.Foundation.NSData
@@ -10,13 +11,14 @@ import platform.Foundation.NSNumber
 import platform.Foundation.NSString
 import platform.Foundation.create
 import platform.Foundation.numberWithInt
+import platform.darwin.NSUInteger
 import platform.posix.memcpy
 
 @OptIn(UnsafeNumber::class)
 fun ByteArray.toNSData(): NSData = memScoped {
     NSData.create(
         bytes = allocArrayOf(this@toNSData),
-        length = this@toNSData.size.toULong()
+        length = this@toNSData.size.convert<NSUInteger>()
     )
 }
 
