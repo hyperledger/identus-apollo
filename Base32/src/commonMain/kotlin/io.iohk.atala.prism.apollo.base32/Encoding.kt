c0ff1f7494d167dd839e8757c35bb8ce61efbd4d
Ahmed Moussa <ahmed.moussa@iohk.io>
2022-12-05 06:13:34 +0200

    feat: Base32
    - Standard
    - Upper
    - Hex
    - Hex Upper

---
+++ Base32/src/commonMain/kotlin/io.iohk.atala.prism.apollo.base32/Encoding.kt
@@ -0,0 +1,66 @@
+package io.iohk.atala.prism.apollo.base32
+
+/**
+ * Base32 encoding scheme
+ *
+ * TODO: Figure out a way to put both type with padded version in on scheme
+ */
+sealed interface Encoding {
+    val alphabet: String
+
+    /**
+     * Base32 Standard
+     */
+    object Standard : Encoding {
+        override val alphabet: String = "abcdefghijklmnopqrstuvwxyz234567"
+    }
+
+    /**
+     * Base32 Standard with padding
+     */
+    object StandardPad : Encoding {
+        override val alphabet: String = "abcdefghijklmnopqrstuvwxyz234567="
+    }
+
+    /**
+     * Base32 Upper
+     */
+    object Upper : Encoding {
+        override val alphabet: String = "ABCDEFGHIJKLMNOPQRSTUVWXYZ234567"
+    }
+
+    /**
+     * Base32 Upper with padding
+     */
+    object UpperPad : Encoding {
+        override val alphabet: String = "ABCDEFGHIJKLMNOPQRSTUVWXYZ234567="
+    }
+
+    /**
+     * Base32 Hex
+     */
+    object Hex : Encoding {
+        override val alphabet: String = "0123456789abcdefghijklmnopqrstuvw"
+    }
+
+    /**
+     * Base32 Hex with padding
+     */
+    object HexPad : Encoding {
+        override val alphabet: String = "0123456789abcdefghijklmnopqrstuvw="
+    }
+
+    /**
+     * Base32 Hex Upper
+     */
+    object HexUpper : Encoding {
+        override val alphabet: String = "0123456789ABCDEFGHIJKLMNOPQRSTUVW"
+    }
+
+    /**
+     * Base32 Hex Upper with padding
+     */
+    object HexUpperPad : Encoding {
+        override val alphabet: String = "0123456789ABCDEFGHIJKLMNOPQRSTUVW="
+    }
+}
