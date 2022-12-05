74e764656fe060870655d53f9d565be066eefb03
Ahmed Moussa <ahmed.moussa@iohk.io>
2022-12-05 04:26:43 +0200

    test: Base16 Upper
    Signed-off-by: Ahmed Moussa <ahmed.moussa@iohk.io>

--- Base16/src/commonTest/kotlin/io.iohk.atala.prism.apollo.base16/Base16Tests.kt
+++ Base16/src/commonTest/kotlin/io.iohk.atala.prism.apollo.base16/Base16Tests.kt
@@ -14,4 +14,14 @@ class Base16Tests {
     fun testDecodeBase16() {
         assertEquals("Welcome to IOG!", "57656C636F6D6520746F20494F4721".lowercase().base16Decoded)
     }
+
+    @Test
+    fun testEncodeBase16Upper() {
+        assertEquals("57656C636F6D6520746F20494F4721", "Welcome to IOG!".base16UpperEncoded)
+    }
+
+    @Test
+    fun testDecodeBase16Upper() {
+        assertEquals("Welcome to IOG!", "57656C636F6D6520746F20494F4721".base16UpperDecoded)
+    }
 }
