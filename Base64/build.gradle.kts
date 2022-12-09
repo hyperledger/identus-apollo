9a992471683aeaa4ae3730e70a377f979ee1a12b
Ahmed Moussa <ahmed.moussa@iohk.io>
2022-12-09 14:46:35 +0200

    refact: Base64
    Re-implemented Base64 and fixed the issue with encoding and decoding

--- Base64/build.gradle.kts
+++ Base64/build.gradle.kts
@@ -100,7 +100,11 @@ kotlin {
     }
 
     sourceSets {
-        val commonMain by getting
+        val commonMain by getting {
+            dependencies {
+                implementation("com.ionspin.kotlin:bignum:0.3.7")
+            }
+        }
         val commonTest by getting {
             dependencies {
                 implementation(kotlin("test"))
