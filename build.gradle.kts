1ca5b687cad998ee5e9d2f163b0a13617cfe602a
Ribó <elribonazo@gmail.com>
2022-12-10 13:08:53 +0100

    fix: Add all the required dependencies for Apollo to build with the native cinterops ios version of secp256k1 ec.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,12 @@
-import org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootExtension
-import org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootPlugin
+version = "1.0.0-alpha"
+group = "io.iohk.atala.prism"
 
 plugins {
+    java
+    kotlin("jvm") version "1.7.22"
+    kotlin("multiplatform") version "1.7.22" apply false
+    kotlin("native.cocoapods") version "1.7.22" apply false
+    kotlin("plugin.serialization") version "1.7.22" apply false
     id("org.jetbrains.dokka") version "1.7.10"
     id("org.jlleitschuh.gradle.ktlint") version "11.0.0"
     id("maven-publish")
@@ -9,25 +14,39 @@ plugins {
 
 buildscript {
     repositories {
-        gradlePluginPortal()
-        google()
         mavenCentral()
+        mavenLocal()
+        google()
+        gradlePluginPortal()
+        maven("https://plugins.gradle.org/m2/")
+        // Needed for Kotlin coroutines that support new memory management mode
+        maven {
+            url = uri("https://maven.pkg.jetbrains.space/public/p/kotlinx-coroutines/maven")
+        }
     }
     dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.20")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.22")
         classpath("com.android.tools.build:gradle:7.2.2")
     }
 }
 
-version = "1.0.0-alpha"
-group = "io.iohk.atala.prism"
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(11))
+        vendor.set(JvmVendorSpec.ADOPTOPENJDK)
+    }
+}
 
 allprojects {
-    group = "io.iohk.atala.prism"
-
     repositories {
-        google()
         mavenCentral()
+        mavenLocal()
+        google()
+        maven("https://plugins.gradle.org/m2/")
+        // Needed for Kotlin coroutines that support new memory management mode
+        maven {
+            url = uri("https://maven.pkg.jetbrains.space/public/p/kotlinx-coroutines/maven")
+        }
     }
 
     val listOfModulesNotToPublish = listOf(
@@ -59,10 +78,6 @@ subprojects {
         verbose.set(true)
         outputToConsole.set(true)
     }
-}
-
-rootProject.plugins.withType(NodeJsRootPlugin::class.java) {
-    rootProject.extensions.getByType(NodeJsRootExtension::class.java).nodeVersion = "16.17.0"
 }
 
 tasks.dokkaGfmMultiModule.configure {
