8e28e3ab339b7d80648495bfad2978c7b0b4073c
Ribó <elribonazo@gmail.com>
2022-12-10 13:08:53 +0100

    fix: Add all the required dependencies for Apollo to build with the native cinterops ios version of secp256k1 ec.
    Co-authored-by: Ahmed Moussa <ahmed.moussa@iohk.io>
    Signed-off-by: Javi Ribo <elribonazo@gmail.com>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,12 +1,31 @@
 pluginManagement {
     repositories {
-        google()
         gradlePluginPortal()
         mavenCentral()
+        google()
+    }
+}
+
+buildscript {
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+        mavenLocal()
+        google()
+        maven("https://plugins.gradle.org/m2/")
+        // Needed for Kotlin coroutines that support new memory management mode
+        maven {
+            url = uri("https://maven.pkg.jetbrains.space/public/p/kotlinx-coroutines/maven")
+        }
+    }
+
+    dependencies {
+        classpath("io.arrow-kt:arrow-ank-gradle:0.11.0")
     }
 }
 
 rootProject.name = "apollo"
+include(":SwiftCryptoKit")
 include(":apollo")
 include(":hashing")
 include(":uuid")
