335535ec0820256c9a4ce2a68a1b18291a1ba7f9
Ahmed Moussa <ahmed.moussa@iohk.io>
2023-01-24 02:17:12 +0200

    Linting update

--- iOSLibs/IOHKRSA/IOHKRSA/Enums/RSAEncryptionType.swift
+++ iOSLibs/IOHKRSA/IOHKRSA/Enums/RSAEncryptionType.swift
@@ -10,12 +10,12 @@ import Foundation
 @objc public enum RSAEncryptionType: Int {
     case rsaEncryption = 1
     case rsaPKCS1
-    case rsaOAEPSHA1
+    case rsaOAEPSHA1 // Weak/Broken Hash Algorithm
     case rsaOAEPSHA224
     case rsaOAEPSHA256
     case rsaOAEPSHA384
     case rsaOAEPSHA512
-    case rsaOAEPSHA1AESGCM
+    case rsaOAEPSHA1AESGCM // Weak/Broken Hash Algorithm
     case rsaOAEPSHA224AESGCM
     case rsaOAEPSHA256AESGCM
     case rsaOAEPSHA384AESGCM
@@ -27,8 +27,8 @@ import Foundation
             return .rsaEncryptionRaw
         case .rsaPKCS1:
             return .rsaEncryptionPKCS1
-        case .rsaOAEPSHA1:
-            return .rsaEncryptionOAEPSHA1
+        case .rsaOAEPSHA1: // Weak/Broken Hash Algorithm
+            return .rsaEncryptionOAEPSHA1 // Weak/Broken Hash Algorithm
         case .rsaOAEPSHA224:
             return .rsaEncryptionOAEPSHA224
         case .rsaOAEPSHA256:
@@ -37,8 +37,8 @@ import Foundation
             return .rsaEncryptionOAEPSHA384
         case .rsaOAEPSHA512:
             return .rsaEncryptionOAEPSHA512
-        case .rsaOAEPSHA1AESGCM:
-            return .rsaEncryptionOAEPSHA1AESGCM
+        case .rsaOAEPSHA1AESGCM: // Weak/Broken Hash Algorithm
+            return .rsaEncryptionOAEPSHA1AESGCM // Weak/Broken Hash Algorithm
         case .rsaOAEPSHA224AESGCM:
             return .rsaEncryptionOAEPSHA224AESGCM
         case .rsaOAEPSHA256AESGCM:
