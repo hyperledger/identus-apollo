b7c6b02e894503b67df770f625c55530b2a1bf1e
Ahmed Moussa <ahmed.moussa@iohk.io>
2023-02-01 19:57:42 +0200

    small fixes
    - fix hashing android unit test manifest package
    - removed commented code from secure-random module js target

--- secure-random/src/jsMain/kotlin/io/iohk/atala/prism/apollo/securerandom/SecureRandom.kt
+++ secure-random/src/jsMain/kotlin/io/iohk/atala/prism/apollo/securerandom/SecureRandom.kt
@@ -11,52 +11,12 @@ actual class SecureRandom actual constructor(
     override fun nextBytes(size: Int): ByteArray {
         val arr = Uint8Array(size)
         return crypto.getRandomValues(arr).buffer.toByteArray()
-//        return if (jsTypeOf(window) != "undefined") { // Browser
-//            (js(
-//                """
-//                    var arr = new Uint32Array(size);
-//                    window.crypto.getRandomValues(arr);
-//                """
-//            ) as Uint32Array).buffer.toByteArray()
-//        } else { // NodeJS
-//            (js(
-//                """
-//                    var crypto = require('crypto');
-//                    var buf = crypto.randomBytes(size);
-//                    var arr = new Uint8Array(size);
-//                    for (var i = 0; i < size; ++i) {
-//                        arr[i] = buf.readUInt8(i)
-//                    }
-//                    arr
-//                """
-//            ) as Uint32Array).buffer.toByteArray()
-//        }
     }
 
     actual companion object : SecureRandomStaticInterface {
         override fun generateSeed(numBytes: Int): ByteArray {
             val arr = Uint8Array(numBytes)
             return crypto.getRandomValues(arr).buffer.toByteArray()
-//            return if (jsTypeOf(window) != "undefined") { // Browser
-//                (js(
-//                    """
-//                        var arr = new Uint32Array(numBytes);
-//                        window.crypto.getRandomValues(arr);
-//                    """
-//                ) as Uint32Array).buffer.toByteArray()
-//            } else { // NodeJS
-//                (js(
-//                    """
-//                        var crypto = require('crypto');
-//                        var buf = crypto.randomBytes(numBytes);
-//                        var arr = new Uint8Array(numBytes);
-//                        for (var i = 0; i < numBytes; ++i) {
-//                            arr[i] = buf.readUInt8(i)
-//                        }
-//                        arr
-//                    """
-//                ) as Uint32Array).buffer.toByteArray()
-//            }
         }
     }
 }
