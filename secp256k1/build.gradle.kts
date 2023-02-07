6809e508d99c6761559a056df956e4bef619d1ce
Ahmed Moussa <ahmed.moussa@iohk.io>
2023-02-07 19:09:39 +0200

    Install Secp256k1 requirements

--- secp256k1/build.gradle.kts
+++ secp256k1/build.gradle.kts
@@ -5,6 +5,15 @@ val bash = "bash"
 
 val buildSecp256k1 by tasks.creating { group = "build" }
 
+val installRequirements by tasks.creating(Exec::class) {
+    group = "build"
+
+    onlyIf { currentOs.isMacOsX }
+
+    workingDir = projectDir
+    commandLine(bash, "macos-install-requirements.sh")
+}
+
 val buildSecp256k1Ios by tasks.creating(Exec::class) {
     group = "build"
     buildSecp256k1.dependsOn(this)
@@ -16,6 +25,10 @@ val buildSecp256k1Ios by tasks.creating(Exec::class) {
 
     workingDir = projectDir
     commandLine(bash, "build-ios.sh")
+
+    doFirst {
+        installRequirements
+    }
 }
 
 val clean by tasks.creating {
---
+++ secp256k1/macos-install-requirements.sh
@@ -0,0 +1,4 @@
+# Install Homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# Install autoconf, automake, libtool
+brew install autoconf automake libtool
