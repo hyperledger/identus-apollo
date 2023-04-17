13aab7a43f34a6b473530ee3c973bfeb8eec1130
Ahmed Moussa <ahmed.moussa@iohk.io>
2023-04-17 11:39:43 +0200

    linting

--- Base64/src/commonMain/kotlin/io/iohk/atala/prism/apollo/base64/Base64.kt
+++ Base64/src/commonMain/kotlin/io/iohk/atala/prism/apollo/base64/Base64.kt
@@ -350,7 +350,7 @@ internal final object Base64 {
                 val n = srclen % 3
                 4 * (srclen / 3) + if (n == 0) 0 else n + 1
             }
-            if (linemax > 0) {// line separators
+            if (linemax > 0) { // line separators
                 len += (len - 1) / linemax * newline!!.size
             }
             return len
