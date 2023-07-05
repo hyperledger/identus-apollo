8ae4c0e5f49c9be71f9f42c4fa03b58119826f31
Curtish <ch@curtish.me>
2023-07-05 12:50:32 +0100

    feat(X25519): implement X25519 jsMain

---
+++ base-asymmetric-encryption/src/commonTest/kotlin/io/iohk/atala/prism/apollo/utils/KMMX25519KeyPairTests.kt
@@ -0,0 +1,15 @@
+package io.iohk.atala.prism.apollo.utils
+
+import kotlin.test.Test
+import kotlin.test.assertNotNull
+
+class KMMX25519KeyPairTests {
+    @Test
+    fun testGenerateKeyPair() {
+        val keyPair = KMMX25519KeyPair.generateKeyPair()
+
+        assertNotNull(keyPair)
+        assertNotNull(keyPair.privateKey)
+        assertNotNull(keyPair.publicKey)
+    }
+}
