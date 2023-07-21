e8945df553f6fdd98bd3ff3be66f21619639b4c6
Curtis <ch@curtish.me>
2023-07-21 16:08:23 +0100

    feat(ED25519): ED25519 jsMain implementation (#76)

--- base-asymmetric-encryption/src/jsMain/kotlin/io/iohk/atala/prism/apollo/utils/KMMEdKeyPair.kt
+++ base-asymmetric-encryption/src/jsMain/kotlin/io/iohk/atala/prism/apollo/utils/KMMEdKeyPair.kt
@@ -1,18 +1,32 @@
 package io.iohk.atala.prism.apollo.utils
 
+import io.iohk.atala.prism.apollo.utils.external.eddsa
+import io.iohk.atala.prism.apollo.utils.external.rand
+import node.buffer.Buffer
+
+@ExperimentalJsExport
+@JsExport
 actual class KMMEdKeyPair actual constructor(
     actual val privateKey: KMMEdPrivateKey,
     actual val publicKey: KMMEdPublicKey
 ) {
-    init {
-        // TODO: we will use this lib for JS https://github.com/indutny/elliptic
-        throw NotImplementedError("Ed25519 is yet to be implemented in JS")
+    actual fun sign(message: ByteArray): ByteArray {
+        return privateKey.sign(message)
+    }
+
+    actual fun verify(message: ByteArray, sig: ByteArray): Boolean {
+        return publicKey.verify(message, sig)
     }
 
     actual companion object : Ed25519KeyPairGeneration {
-        override fun generateEd25519KeyPair(): KMMEdKeyPair {
-            // TODO: we will use this lib for JS https://github.com/indutny/elliptic
-            throw NotImplementedError("Ed25519 is yet to be implemented in JS")
+        override fun generateKeyPair(): KMMEdKeyPair {
+            val ed25519 = eddsa("ed25519")
+            val rnd = rand(32)
+            val secret = Buffer.from(rnd).toByteArray()
+            val keypair = ed25519.keyFromSecret(secret)
+            val public = keypair.getPublic()
+
+            return KMMEdKeyPair(KMMEdPrivateKey(secret), KMMEdPublicKey(public))
         }
     }
 }
