45a1d1fe19c772ac54f4b0f5b07a4a5d5f1a4115
goncalo-frade-iohk <goncalo-frade-iohk@users.noreply.github.com>
2023-10-17 16:23:11 +0000

    chore: prepare release 1.0.3
    Co-authored-by: Ahmed Moussa <ahmed.moussa@iohk.io>
    Signed-off-by: Goncalo Frade <goncalo.frade@iohk.io>

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.7
 import PackageDescription
 
 let package = Package(
@@ -23,8 +23,8 @@ let package = Package(
         // RELEASE
         .binaryTarget(
             name: "Apollo",
-            url: "https://github.com/input-output-hk/atala-prism-apollo/releases/download/<ref>/Apollo.xcframework.zip",
-            checksum: "<checksum>"
+            url: "https://github.com/input-output-hk/atala-prism-apollo/releases/download/1.0.3/Apollo.xcframework.zip",
+            checksum: "89989b558887431844d5395c845a098678577b3c1dc867146ca058f0ce9e63dd"
         )
     ]
 )
