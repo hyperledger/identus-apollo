49d4913d012024171f7f78b30c48f04f8a237a18
Goncalo-FradeIOHK <goncalo.frade@iohk.io>
2023-10-17 15:01:02 +0100

    ci: add initial swift package
    Co-authored-by: Ahmed Moussa <ahmed.moussa@iohk.io>
    Signed-off-by: Goncalo Frade <goncalo.frade@iohk.io>

---
+++ Package.swift
@@ -0,0 +1,30 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "Apollo",
+    platforms: [
+        .iOS(.v13),
+        .macOS(.v11)
+    ],
+    products: [
+        .library(
+            name: "Apollo",
+            targets: ["Apollo"]
+        ),
+    ],
+    targets: [
+        // LOCAL
+        // .binaryTarget(
+        //     name: "Apollo",
+        //     path: "./Apollo.xcframework"
+        // ),
+
+        // RELEASE
+        .binaryTarget(
+            name: "Apollo",
+            url: "https://github.com/input-output-hk/atala-prism-apollo/releases/download/<ref>/Apollo.xcframework.zip",
+            checksum: "<checksum>"
+        )
+    ]
+)
