af6df3df886c5afe4e9f938720bb01eda47004ae
Goncalo-FradeIOHK <goncalo.frade@iohk.io>
2023-10-18 15:55:57 +0100

    feat(mnemonic): js doesnt handle List and swift doesnt handle Array this is the solution

---
+++ base-asymmetric-encryption/src/androidMain/kotlin/io/iohk/atala/prism/apollo/derivation/MnemonicCode.kt
@@ -0,0 +1,13 @@
+package io.iohk.atala.prism.apollo.derivation
+
+final class MnemonicCode constructor(val words: List<String>) {
+    init {
+        if (words.size % 3 != 0) {
+            throw MnemonicLengthException("Can't create a DID from mnemonic that is not dividable by 3")
+        }
+    }
+
+    fun toMnemonic(entropy: ByteArray): List<String> {
+        return MnemonicHelper.toMnemonicCode(words, entropy)
+    }
+}
