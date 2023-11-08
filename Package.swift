6b6fcbcdc3275298d8a92b1dde763157a129aa27
hamada147 <hamada147@users.noreply.github.com>
2023-11-08 11:26:46 +0000

    chore: prepare release 1.0.6

--- Package.swift
+++ Package.swift
@@ -24,7 +24,7 @@ let package = Package(
         .binaryTarget(
             name: "ApolloBinary",
             url: "https://github.com/input-output-hk/atala-prism-apollo/releases/download/1.0.6/Apollo.xcframework.zip",
-            checksum: "647ca0fc88878545eb80981e62509ff465bf16d8bd951fe7019587a0ad51dd24"
+            checksum: "42ac3de033b98153161d5fa8d969a088e3f38d1b75dc309a20351dd80810d88b"
         )
     ]
 )
