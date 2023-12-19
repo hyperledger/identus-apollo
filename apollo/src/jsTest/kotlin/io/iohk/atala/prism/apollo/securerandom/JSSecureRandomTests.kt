3ef02feaff43a5203a0b3a209cb8da88df6a54df
Ahmed Moussa <ahmed.moussa@iohk.io>
2023-12-19 17:10:38 +0200

    fix: crypto is not defined (#136)

---
+++ apollo/src/jsTest/kotlin/io/iohk/atala/prism/apollo/securerandom/JSSecureRandomTests.kt
@@ -0,0 +1,16 @@
+package io.iohk.atala.prism.apollo.securerandom
+
+import kotlin.test.Test
+import kotlin.test.fail
+
+class JSSecureRandomTests {
+    @Test
+    fun test_SecureRandom_not_failing() {
+        try {
+            SecureRandom().nextBytes(16)
+            SecureRandom.generateSeed(16)
+        } catch (ex: Exception) {
+            fail(ex.message)
+        }
+    }
+}
