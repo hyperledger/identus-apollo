3824a988de1a422f9518e51ab784b3e6dc9d101d
Ahmed Moussa <ahmed.moussa@iohk.io>
2023-12-27 21:39:38 +0200

    refactor: Change PBKDF2SHA512 from class to Object (#139)
    Signed-off-by: Ahmed Moussa <ahmed.moussa@iohk.io>

--- apollo/src/androidMain/kotlin/io/iohk/atala/prism/apollo/hashing/PBKDF2SHA512.kt
+++ apollo/src/androidMain/kotlin/io/iohk/atala/prism/apollo/hashing/PBKDF2SHA512.kt
@@ -7,24 +7,22 @@ import org.bitcoinj.crypto.PBKDF2SHA512
  * deriving a key from a password using the PBKDF2 algorithm with SHA-512
  * hash function.
  */
-actual class PBKDF2SHA512 {
-    actual companion object {
-        /**
-         * Derives a key from a password using the PBKDF2 algorithm with SHA-512 hash function.
-         *
-         * @param p The password used for key derivation.
-         * @param s The salt value.
-         * @param c The iteration count.
-         * @param dkLen The desired length of the derived key in bytes.
-         * @return The derived key as a ByteArray.
-         */
-        actual fun derive(
-            p: String,
-            s: String,
-            c: Int,
-            dkLen: Int
-        ): ByteArray {
-            return PBKDF2SHA512.derive(p, s, c, dkLen)
-        }
+actual object PBKDF2SHA512 {
+    /**
+     * Derives a key from a password using the PBKDF2 algorithm with SHA-512 hash function.
+     *
+     * @param p The password used for key derivation.
+     * @param s The salt value.
+     * @param c The iteration count.
+     * @param dkLen The desired length of the derived key in bytes.
+     * @return The derived key as a ByteArray.
+     */
+    actual fun derive(
+        p: String,
+        s: String,
+        c: Int,
+        dkLen: Int
+    ): ByteArray {
+        return PBKDF2SHA512.derive(p, s, c, dkLen)
     }
 }
--- apollo/src/commonMain/kotlin/io/iohk/atala/prism/apollo/hashing/PBKDF2SHA512.kt
+++ apollo/src/commonMain/kotlin/io/iohk/atala/prism/apollo/hashing/PBKDF2SHA512.kt
@@ -5,17 +5,15 @@ package io.iohk.atala.prism.apollo.hashing
  * deriving a key from a password using the PBKDF2 algorithm with SHA-512
  * hash function.
  */
-expect class PBKDF2SHA512 {
-    companion object {
-        /**
-         * Derives a key from a password using the PBKDF2 algorithm with SHA-512 hash function.
-         *
-         * @param p The password used for key derivation.
-         * @param s The salt value.
-         * @param c The iteration count.
-         * @param dkLen The desired length of the derived key in bytes.
-         * @return The derived key as a ByteArray.
-         */
-        fun derive(p: String, s: String, c: Int, dkLen: Int): ByteArray
-    }
+expect object PBKDF2SHA512 {
+    /**
+     * Derives a key from a password using the PBKDF2 algorithm with SHA-512 hash function.
+     *
+     * @param p The password used for key derivation.
+     * @param s The salt value.
+     * @param c The iteration count.
+     * @param dkLen The desired length of the derived key in bytes.
+     * @return The derived key as a ByteArray.
+     */
+    fun derive(p: String, s: String, c: Int, dkLen: Int): ByteArray
 }
