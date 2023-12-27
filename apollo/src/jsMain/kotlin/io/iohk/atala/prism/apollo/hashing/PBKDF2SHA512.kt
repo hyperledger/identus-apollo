da7e6fe801bbd7518c083eb1dce4ddadc770eed4
Ahmed Moussa <ahmed.moussa@iohk.io>
2023-12-27 21:39:38 +0200

    refactor: Change PBKDF2SHA512 from class to Object (#139)
    Signed-off-by: Ahmed Moussa <ahmed.moussa@iohk.io>

--- apollo/src/jsMain/kotlin/io/iohk/atala/prism/apollo/hashing/PBKDF2SHA512.kt
+++ apollo/src/jsMain/kotlin/io/iohk/atala/prism/apollo/hashing/PBKDF2SHA512.kt
@@ -10,33 +10,31 @@ import org.khronos.webgl.get
  * deriving a key from a password using the PBKDF2 algorithm with SHA-512
  * hash function.
  */
-actual class PBKDF2SHA512 {
-    actual companion object {
-        /**
-         * Derives a key from a password using the PBKDF2 algorithm with SHA-512 hash function.
-         *
-         * @param p The password used for key derivation.
-         * @param s The salt value.
-         * @param c The iteration count.
-         * @param dkLen The desired length of the derived key in bytes.
-         * @return The derived key as a ByteArray.
-         */
-        actual fun derive(
-            p: String,
-            s: String,
-            c: Int,
-            dkLen: Int
-        ): ByteArray {
-            val opts = js("{}")
-            opts.c = 2048
-            opts.dkLen = 64
-            val derived = pbkdf2(sha512, p, s, opts).buffer
-            val uint8Array = Uint8Array(derived)
-            val byteArray = ByteArray(uint8Array.length)
-            for (i in 0 until uint8Array.length) {
-                byteArray[i] = uint8Array[i].toByte()
-            }
-            return byteArray
+actual object PBKDF2SHA512 {
+    /**
+     * Derives a key from a password using the PBKDF2 algorithm with SHA-512 hash function.
+     *
+     * @param p The password used for key derivation.
+     * @param s The salt value.
+     * @param c The iteration count.
+     * @param dkLen The desired length of the derived key in bytes.
+     * @return The derived key as a ByteArray.
+     */
+    actual fun derive(
+        p: String,
+        s: String,
+        c: Int,
+        dkLen: Int
+    ): ByteArray {
+        val opts = js("{}")
+        opts.c = 2048
+        opts.dkLen = 64
+        val derived = pbkdf2(sha512, p, s, opts).buffer
+        val uint8Array = Uint8Array(derived)
+        val byteArray = ByteArray(uint8Array.length)
+        for (i in 0 until uint8Array.length) {
+            byteArray[i] = uint8Array[i].toByte()
         }
+        return byteArray
     }
 }
