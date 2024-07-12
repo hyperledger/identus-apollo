a0266bfd846ff1a07aba7eaaded4a2ac2d545421
Javier Ribó <elribonazo@gmail.com>
2024-07-12 12:23:38 +0200

    fix: linter issues Signed-off-by: Javier Ribó <elribonazo@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,7 +23,6 @@ buildscript {
                 password = project.findProperty("sonatypePassword") as String? ?: System.getenv("OSSRH_TOKEN")
             }
         }
-
     }
     dependencies {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.23")
@@ -183,7 +182,6 @@ nexusPublishing {
             snapshotRepositoryUrl.set(uri("https://oss.sonatype.org/content/repositories/snapshots/"))
             username.set(System.getenv("OSSRH_USERNAME"))
             password.set(System.getenv("OSSRH_TOKEN"))
-
         }
     }
 }
